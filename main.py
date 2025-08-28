ad50f9895b51499a309fa1de6952815661f70bdf
LLLLLLLccWWW <a0989328529@gmail.com>
2025-08-28 21:49:45 +0800

    coding at 0828

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI,Form  
+from fastapi import FastAPI,Form,requests  
 from routers.api_v1.routers import router
 from config import Settings
+import time
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(root_path="/ithome")
 app.include_router(router, prefix="/api/v1")
@@ -24,6 +26,34 @@ async def get_info():
         "version": settings.version,
         "description": settings.description
     }
+
+@app.middleware("http")
+async def add_process_time_header(request: requests.Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
+
+@app.get("/hello")
+def say_hello():
+    return {"message": "Hello World"}
+
+origins = [
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.get('/')
+async def root():
+    return {"message": "Hello World from root"}
 
 # fake_items_db = [{"item_name": "Item 1"}, {"item_name": "Item 2"}, {"item_name": "Item 3"}]
 
